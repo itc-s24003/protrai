8e25287e08d9c00f769b1a17540dd1684b1488c1
itc-s24003 <itc-s24003@std.it-college.ac.jp>
2024-07-01 15:18:03 +0900

    entry_kadai.pyの提出

--- entry_kadai.py
+++ entry_kadai.py
@@ -7,13 +7,13 @@ def dispLabel():
     # entryメソッドを使用して入力を受付変数aに格納
     a = entry.get()
     print(f"aは{type(a)}") # ログの出力
-    lbl.config(text=f"{a}さんこんにちは")
+    lbl.config(text=int(a)*1.1)
 
 root = tk.Tk()
 root.title("エントリーウィジェット")
 root.geometry("400x200")
 
-lbl = tk.Label(text="ラベル", font=("Helvetica", 20))
+lbl = tk.Label(text="金額を入力してください", font=("Helvetica", 20))
 entry = tk.Entry(font=("Helvetica", 20))
 btn = tk.Button(text="ボタン", font=("Helvetica", 20), command=dispLabel)
 
